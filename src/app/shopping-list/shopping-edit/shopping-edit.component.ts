ffca297b2e344edbda04a7619c62440891746a22
Tarun Jangra <tarun@izap.in>
2019-11-23 00:45:45 -0800

    get value from input through viewchild

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, ViewChild, Output} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'rb-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput',{static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput',{static: false}) amountInputRef: ElementRef;
+
+  @Output() ingredientAdded: EventEmitter = new EventEmitter<{name: string, amount: number}>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdd(amt: HTMLInputElement){
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value)
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
