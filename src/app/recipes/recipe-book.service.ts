e5f971e49446a75782d41715d8669c9a1ae6283d
Tarun Jangra <tarun@izap.in>
2019-11-29 04:26:37 -0800

    added one service to other

--- src/app/recipes/recipe-book.service.ts
+++ src/app/recipes/recipe-book.service.ts
@@ -1,13 +1,30 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeBookService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is test','https://realhousemoms.com/wp-content/uploads/One-Pot-Alfredo-Pasta-Easy-Dinner-Recipe-HERO-600x900.jpg'),
-    new Recipe('A Test Recipe','This is test','https://realhousemoms.com/wp-content/uploads/One-Pot-Alfredo-Pasta-Easy-Dinner-Recipe-HERO-600x900.jpg')
+    new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty schnitzel - just awesome',
+        'https://www.crushpixel.com/static12/preview2/stock-photo-tasty-schnitzel-in-breading-with-fried-potato-onion-and-mushrooms-859791.jpg',
+         [
+             new Ingredient('Meat', 1),
+             new Ingredient('French Fries', 20)
+         ]
+    ),
+    new Recipe(
+        'Big fat burger',
+        'What else you need to say?',
+        'https://cdn.newsapi.com.au/image/v1/dfd657934bf7bf648edbdc19670bf977?width=650',
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1)
+        ]
+        )
   ];
 
 
