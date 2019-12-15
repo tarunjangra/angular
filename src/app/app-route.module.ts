5e0ffe108532ec5fd573d1fb523eeead0f6db689
Tarun Jangra <tarun@izap.in>
2019-12-15 15:45:32 -0800

    let's make the preloading strategy to PreloadAllModules

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
 
@@ -12,7 +12,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 
